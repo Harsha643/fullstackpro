ccf3f5fd74e14da49081982f4f90c53b44dbc299
Harsha643 <battuharsha6@gmail.com>
2025-05-12 17:11:00 +0530

    this

--- backend/Students/models/studentAssignmentmodels.js
+++ backend/Students/models/studentAssignmentmodels.js
@@ -1,8 +1,7 @@
 const  mongoose = require('mongoose');
 
 const studdentSubmissionSchema = new mongoose.Schema({
-    studentId: { type: String, required: true },
-    assignmentId: { type: String, required: true },
+    
     submissionDate: { type: Date, default: Date.now },
     fileUrl: { type: String, required: true }
 
